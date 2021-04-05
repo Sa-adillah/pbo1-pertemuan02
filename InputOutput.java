8606791d57f7e98b59ffdebabaa1f0311f822f6f
Sa-adillah <63109326+Sa-adillah@users.noreply.github.com>
2021-04-05 12:57:06 +0800

    Membuat class inputan

--- InputOutput.java
+++ InputOutput.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class InputOutput{
  public static void main(String [] args){
+  String nama;
+  int usia;
+  double ipk;
+
+  Scanner inputan = new Scanner(System.in);
+
+  System.out.print("Input nama: ");
+  nama = inputan.nextLine();
+
+  System.out.print("Input usia: ");
+  usia = inputan.nextInt();
+
+  System.out.print("Input ipk: ");
+  ipk = inputan.nextDouble();
+
+  System.out.println("Hai "+nama);
+  System.out.println("Usia: "+usia);
+  System.out.println("ipk: "+ipk);
+
 
  }
 }
